66fdd02aab4ad4cd3fa10e8f7e263855185ab1fd
chowcli <136085102+chowcli@users.noreply.github.com>
2024-10-23 09:04:55 +0700

    Finish Exercise 1

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
+
+function Option({ opt, selectVariant, handleSelect }) {
+  const id = `variant-${opt}`;
+
+  return (
+    <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+      <label htmlFor={id}>
+        <input
+          id={id}
+          type="radio"
+          name="variant"
+          value={opt}
+          checked={opt === selectVariant}
+          onChange={handleSelect}
+        />
+        {opt}
+      </label>
+    </div>
+  );
+}
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState("");
+  const [variant, setVariant] = React.useState("notice");
+
+  const handleChangeMessage = event => setMessage(event.target.value);
+  const handleSelectVariant = event => setVariant(event.target.value);
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -16,42 +42,24 @@ function ToastPlayground() {
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
-          <label
-            htmlFor="message"
-            className={styles.label}
-            style={{ alignSelf: 'baseline' }}
-          >
+          <label htmlFor="message" className={styles.label} style={{ alignSelf: "baseline" }}>
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput} value={message} onChange={handleChangeMessage} />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
-          </div>
+          {VARIANT_OPTIONS.map(opt => (
+            <Option key={opt} opt={opt} selectVariant={variant} handleSelect={handleSelectVariant} />
+          ))}
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
