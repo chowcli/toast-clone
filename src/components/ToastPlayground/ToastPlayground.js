92043b3479169c3254ceff9d8d35e266ac2ac97c
chowcli <136085102+chowcli@users.noreply.github.com>
2024-10-24 09:20:40 +0700

    Finish requirement #4 in Exercise 3. Finish all requirement in Exercise 3.
    Find hidden bug: call updater function inside an
    updater function.
    The reason why in the previous version which use
    onClick it doesn't appear to have bug it's because
    of React batching state update perhaps prevent it.
    However when switch to use handleSubmit the bug
    arose.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -8,7 +8,7 @@ import styles from "./ToastPlayground.module.css";
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function Option({ opt, selectVariant, handleSelect }) {
-  // console.log("Option re-render");
+  console.log("Option re-render");
 
   const id = `variant-${opt}`;
 
@@ -46,7 +46,19 @@ function ToastPlayground() {
     return { shelf, setShelf };
   }, [shelf]);
 
-  const handleChangeMessage = event => setMessage(event.target.value);
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    setShelf(value => {
+      const id = crypto.randomUUID().substring(7);
+      const newValue = [...value, { id, variant, message }];
+
+      return newValue;
+    });
+
+    setMessage("");
+    setVariant("notice");
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -59,42 +71,37 @@ function ToastPlayground() {
         <ToastShelf></ToastShelf>
       </ShelfContext.Provider>
 
-      <div className={styles.controlsWrapper}>
-        <div className={styles.row}>
-          <label htmlFor="message" className={styles.label} style={{ alignSelf: "baseline" }}>
-            Message
-          </label>
-          <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} value={message} onChange={handleChangeMessage} />
+      <form onSubmit={handleSubmit}>
+        <div className={styles.controlsWrapper}>
+          <div className={styles.row}>
+            <label htmlFor="message" className={styles.label} style={{ alignSelf: "baseline" }}>
+              Message
+            </label>
+            <div className={styles.inputWrapper}>
+              <textarea
+                id="message"
+                className={styles.messageInput}
+                value={message}
+                onChange={event => setMessage(event.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label}>Variant</div>
+            {VARIANT_OPTIONS.map(opt => (
+              <PureOption key={opt} opt={opt} selectVariant={variant} handleSelect={handleSelectVariant} />
+            ))}
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label} />
+            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+              <Button>Pop Toast!</Button>
+            </div>
           </div>
         </div>
-
-        <div className={styles.row}>
-          <div className={styles.label}>Variant</div>
-          {VARIANT_OPTIONS.map(opt => (
-            <PureOption key={opt} opt={opt} selectVariant={variant} handleSelect={handleSelectVariant} />
-          ))}
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.label} />
-          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button
-              onClick={() =>
-                setShelf(value => {
-                  const id = crypto.randomUUID().substring(7);
-                  const newValue = [...value, { id, variant, message }];
-
-                  setShelf(newValue);
-                  setMessage("");
-                  setVariant("notice");
-                })
-              }>
-              Pop Toast!
-            </Button>
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
